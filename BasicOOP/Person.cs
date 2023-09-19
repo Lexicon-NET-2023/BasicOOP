c1ca9a8b22edfcca3940dd4b9821040e7b48d771
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-09-19 13:48:13 +0200

    IPerson, Nullable Department in Admin

--- BasicOOP/Person.cs
+++ BasicOOP/Person.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace BasicOOP
 {
-    internal class Person
+
+    internal interface IPerson
+    {
+        string Name { get; set; }
+        void Do();
+    }
+
+    internal class Person : IPerson
     {
         public string Name { get; set; } = "";// string.Empty;
         //more props
+
+        public void Do()
+        {
+            Console.WriteLine("Person d");
+        }
     }
 
     internal class Employee  : Person
@@ -19,6 +31,6 @@ namespace BasicOOP
     
     internal class Admin  : Employee
     {
-        public string Department { get; set; }
+        public string? Department { get; set; }
     }
 }
--- BasicOOP/Program.cs
+++ BasicOOP/Program.cs
@@ -11,12 +11,15 @@
             Employee admin = new Admin();
             Person admin2 = new Admin();
             Admin admin3 = new Admin();
+            string departmentName = admin3.Department;
+            var length = departmentName.Length;
+            
 
             Do(admin3);
             Do(admin2);
             Do(admin);
 
-            var persons = new List<Person>()
+            var persons = new List<IPerson>()
             {
                 person,
                 admin2,
