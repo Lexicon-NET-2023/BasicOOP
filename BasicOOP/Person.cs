a3bef42da6ed7c5fe5b970139b4183895a81a9c2
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-09-19 13:40:33 +0200

    Person

---
+++ BasicOOP/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOOP
+{
+    internal class Person
+    {
+        public string Name { get; set; } = "";// string.Empty;
+        //more props
+    }
+
+    internal class Employee  : Person
+    {
+        public int Salary { get; set; }
+    } 
+    
+    internal class Admin  : Employee
+    {
+        public string Department { get; set; }
+    }
+}
--- BasicOOP/Program.cs
+++ BasicOOP/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Person person = new Employee() {Name = "Nisse", Salary = 25000 };
+            person.Name = "Kalle";
+
+
+            Employee admin = new Admin();
+            Person admin2 = new Admin();
+            Admin admin3 = new Admin();
+
+            Do(admin3);
+            Do(admin2);
+            Do(admin);
+
+            var persons = new List<Person>()
+            {
+                person,
+                admin2,
+                admin3,
+                admin
+            };
+            
+        }
+
+
+        public static void Do(Person person)
+        {
+            Console.WriteLine(person);
         }
     }
 }
